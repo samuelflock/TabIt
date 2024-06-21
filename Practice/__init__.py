896f75424b22725a1b9f501776842508167e58c3
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-06-21 11:13:25 -0600

    Practice ML

---
+++ Practice/__init__.py
@@ -0,0 +1,1 @@
+# This file makes SRC a Python Module
---
+++ src/train.py
@@ -0,0 +1,8 @@
+import torch
+import torchaudio
+
+print(torch.__version__)
+print(torchaudio.__version__)
+
+import matplotlib.pyplot as plt
+
