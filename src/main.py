3dcc8bacf376b30ac8d5dd9ecd5e84353dd489ee
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-07-26 14:16:03 -0600

    Basic CLI Complete

--- src/main.py
+++ src/main.py
@@ -1,14 +1,70 @@
+
+
+
+
+# import modules used here -- sys is a very standard one
+import sys, argparse, logging
 import demucs.separate
-import demucs
 import pytube
 
-#demucs.separate.main(["--mp3", "-n", "mdx_extra", "./music/FileName.mp3"])
-demucs.separate.main(["--mp3", "-n", "htdemucs_6s", "./music/FileName.mp3"])
+# Gather our code in a main() function
+def main(args, loglevel):
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
 
-# separator = demucs.Separator()
+    logging.info("Beginning Seperation")
+    logging.debug("Chosen Source: %s" % args.source)
+    logging.debug("Chosen Model: %s" % args.model)
+  
+    #demucs.separate.main(["--mp3", "-n", "mdx_extra", "./music/FileName.mp3"])
+    #demucs.separate.main(["--mp3", "-n", "htdemucs_6s", "./music/FileName.mp3"])
+    demucs.separate.main(["--mp3", "-n", args.model, args.source])
 
-# origin, separated = separator.separate_audio_file("~/Downloads/FileName.mp3")
+    logging.info("Completed Seperation")
 
-# for file, sources in separated:
-#     for stem, source in sources.items():
-#         demucs.api.save_audio(source, f"{stem}_{file}", samplerate=separator.samplerate)
+    # separator = demucs.Separator()
+
+    # origin, separated = separator.separate_audio_file("~/Downloads/FileName.mp3")
+
+    # for file, sources in separated:
+    #     for stem, source in sources.items():
+    #         demucs.api.save_audio(source, f"{stem}_{file}", samplerate=separator.samplerate)
+    
+    
+ 
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( 
+                                    prog='TabIt',
+                                    description = 'Converts Audio into Guitar Tablature',
+                                    epilog = "Epilogue to help (TODO: Update Later)")
+    
+    # Arguments (Source, Model, Verbose)
+    parser.add_argument('source',
+                        type=str,
+                        help='requires mp3 file path')
+    parser.add_argument("-m", "--model",
+                        type=str,
+                        default='htdemucs_6s',
+                        help='list of available models in TODO')
+    parser.add_argument(
+                      "-v",
+                      "--verbose",
+                      help="increase output verbosity",
+                      action="store_true")
+    
+    # Read User Arguments
+    try:
+        args = parser.parse_args()
+
+        # Setup logging
+        if args.verbose:
+            loglevel = logging.DEBUG
+        else:
+            loglevel = logging.INFO
+    
+        main(args, loglevel)
+
+    except:
+        pass
+  
